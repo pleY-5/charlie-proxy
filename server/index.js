4d31db07fb01fd49d22d67ee3a935f25676a7ba7
Charlie Phan <charliegphan@gmail.com>
2018-10-11 08:58:23 -0700

    use deployed service link

--- server/index.js
+++ server/index.js
@@ -12,8 +12,8 @@ app.use('/:id', express.static('public'));
 
 app.set('port', process.env.PORT || 8080);
 
-app.get('/api/reviews/:nameOrId', proxy(''));
-app.post('/api/reviews', proxy('http://localhost:3002'));
+app.get('/api/reviews/:nameOrId', proxy('ec2-34-216-225-31.us-west-2.compute.amazonaws.com'));
+app.post('/api/reviews', proxy('ec2-34-216-225-31.us-west-2.compute.amazonaws.com'));
 
 app.listen(app.get('port'), () => {
   console.log(`app is listening to port ${app.get('port')}`);
